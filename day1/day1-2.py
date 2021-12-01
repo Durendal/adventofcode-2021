efc0005405ae6edaa90ac1f0162f0d20d018d7d5
Durendal <sir.durendal@gmail.com>
2021-12-01 01:15:05 -0600

    Changed to list slice

--- day1/day1-2.py
+++ day1/day1-2.py
@@ -1,6 +1,6 @@
 def main():
     d = [int(i) for i in open('input.txt', 'r').readlines()]
-    print(sum([(sum(d[i:i+3]) < sum(d[i+1:i+4])) for i in range(0, len(d)-3)]))
+    print(sum([sum(d[i:i+3]) < sum(d[i+1:i+4]) for i in range(0, len(d)-3)]))
 
 if __name__ == '__main__':
     main()

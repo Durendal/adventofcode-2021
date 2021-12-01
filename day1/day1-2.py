a41d6e2f0d2198075e05561273e5ec70de99ec8e
Durendal <sir.durendal@gmail.com>
2021-12-01 17:11:38 -0600

    Simplified calculations

--- day1/day1-2.py
+++ day1/day1-2.py
@@ -1,6 +1,6 @@
 def main():
     d = [int(i) for i in open('input.txt', 'r').readlines()]
-    print(sum([sum(d[i:i+3]) < sum(d[i+1:i+4]) for i in range(0, len(d)-3)]))
+    print(sum([d[i] < d[i+3] for i in range(0, len(d)-3)]))
 
 if __name__ == '__main__':
     main()

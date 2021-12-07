997573d325481ed06ca4c96f1b3667082ec3aba9
Durendal <sir.durendal@gmail.com>
2021-12-07 00:01:34 -0600

    Added Day 7 problem 1

---
+++ day7/day7-1.py
@@ -0,0 +1,5 @@
+from statistics import mean, median, mode
+data = [int(i.strip()) for i in open('input2.txt', 'r').read().split(",")]
+bases = [round(i) for i in [mean(data), median(data), mode(data)]]
+print(int(min([sum([abs(i-base) for i in data]) for base in bases])))
+
